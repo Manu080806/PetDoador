09e1bcc420284ccdb8b8d26a7df5835ed0559854
Manu080806 <emanuss2006@gmail.com>
2025-06-24 22:41:37 -0300

    pc novo

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
     $dbHost = '127.0.0.1:3307';
     $dbUsername = 'root';
-    $dbPassword = 'novosql';
+    $dbPassword = 'novosql123';
     $dbName = 'tccpetdoador';
 
         $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
